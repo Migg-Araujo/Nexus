ee2eefbfed15da63bfbe2880ea093058004136aa
Miguel Araújo <miguelbzr6@gmail.com>
2024-10-24 23:29:50 -0300

    add some animations && page 404

--- pages/404.php
+++ pages/404.php
@@ -1,5 +1,9 @@
 <?php include 'header.php';?>
 <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>css/404.css">
-<h1>Error 404</h1>
-<h3>Page not Found</h2>
-<a href="<?php echo INCLUDE_PATH; ?>">Home Page</a>
+<div class="icon">
+    <div class="logo"></div>
+    <div class="name"></div>
+</div>
+<h1>Erro 404</h1>
+<h3>Página não encontrada</h2>
+<a href="<?php echo INCLUDE_PATH; ?>">Voltar ao Início</a>
