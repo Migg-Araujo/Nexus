7e1cebd41dafd593bf070015ec54246235b63ee3
Miguel Araújo <miguelbzr6@gmail.com>
2024-10-25 11:44:37 -0300

    edit: local database

--- config/sql/connection.php
+++ config/sql/connection.php
@@ -1,7 +1,7 @@
 <?php
-    $host = getenv('DB_HOST');
-    $username = getenv('DB_USERNAME');
-    $password = getenv('DB_PASSWORD');
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
 
     $conn = mysqli_connect($host, $username, $password);
 
