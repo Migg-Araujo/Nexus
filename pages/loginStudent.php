6bd8a75b63210255df3579ed54ab6587465f9c52
Miguel Araújo <miguelbzr6@gmail.com>
2024-11-05 11:47:54 -0300

    feat: add login page

--- pages/loginStudent.php
+++ pages/loginStudent.php
@@ -1,3 +1,8 @@
+<script>
+    var titleElement = document.querySelector('title');
+    titleElement.innerText = "Nexus | Área do Aluno ";
+</script>
+
 <?php
     if(isset($_SESSION['user'])){
         header('Location: user');
@@ -32,19 +37,12 @@
 <main>
     <div class="login-area">
         <div class="close" onclick="window.location.href='<?php echo INCLUDE_PATH; ?>'"></div>
-        <div class="sign-in-area">
-                <h1>Sign In</h1>
+                <h1>Área do Aluno</h1>
                 <form action="?" method="POST">
-                    <input type="text" placeholder="User or Email" autocomplete="off" required name='userEmail'>
-                    <input type="password" placeholder="Password" autocomplete="off" required name='password'>
-                    <a href="">Forget Your Password?</a>
-                    <input type="submit" value="SIGN IN">
+                    <input type="text" placeholder="Email" autocomplete="off" required name='userEmail'>
+                    <input type="password" placeholder="Senha" autocomplete="off" required name='password'>
+                    <a href="">Esqueceu sua senha?</a>
+                    <input type="submit" value="Entrar">
                 </form>
-        </div>
-        <div class="sign-up-area">
-            <h1>Sign Up</h1>
-            <p>Don't have an account?</p>
-            <button onclick="<?php echo "window.location.href = '".INCLUDE_PATH."signup'";?>">SIGN UP</button>
-        </div>
     </div>
 </main>
--- pages/profile.php
+++
@@ -1,7 +0,0 @@
-<?php include 'header.php';
-if(!isset($_SESSION['user'])){
-    header('Location: login');
-}
-?>
-<main></main>
-<?php include 'footer.php';?>
